18e3b7aeb9e05e1f6f4d017c5f040e6ded94e2fe
Sergey Borisov <borisovsergei113@mail.ru>
2021-09-26 15:05:44 +0300

    add class CannyLicenseNumberSearcher

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import imutils
 from skimage.segmentation import clear_border
 
 
-class LicenseNumberSearcher:
+class MorphologicalLicenseNumberSearcher:
     def __init__(self, debug=False):
         self.debug = debug
         self.minAR = 4  # отношение длины/ширины российского номера
@@ -97,15 +97,69 @@ class LicenseNumberSearcher:
         return regionOfInterest, finalLicensePlateContour
 
 
-if __name__ == "__main__":
-    NumberSearcher = LicenseNumberSearcher(debug=True)
+class CannyLicenseNumberSearcher:
+    def __init__(self):
+        self.debug = True
+        pass
 
-    filename = os.path.join(os.path.dirname(__file__), "objects-sources/img_5.png")
+    def debugImageShow(self, title, image, waitKey=True):
+        if self.debug:
+            cv2.imshow(title, image)
+            if waitKey:
+                cv2.waitKey(0)
+
+    def locateLicensePlate(self, gray, img):
+        gray = cv2.bilateralFilter(gray, 13, 15, 15)
+        self.debugImageShow('gray', gray)
+        edged = cv2.Canny(gray, 30, 200)  # Perform Edge detection
+        self.debugImageShow('edged', edged)
+
+        contours = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours = imutils.grab_contours(contours)
+        contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
+        screenCnt = None
+
+        for c in contours:
+            # approximate the contour
+            peri = cv2.arcLength(c, True)
+            approx = cv2.approxPolyDP(c, 0.018 * peri, True)
+            # if our approximated contour has four points, then
+            # we can assume that we have found our screen
+            if len(approx) == 4:
+                screenCnt = approx
+                break
+
+        if screenCnt is None:
+            print("No contour detected")
+            return
+
+        cv2.drawContours(img, [screenCnt], -1, (0, 0, 255), 3)
+        self.debugImageShow('contour', img)
+
+        mask = np.zeros(gray.shape, np.uint8)
+        new_image = cv2.drawContours(mask, [screenCnt], 0, 255, -1, )
+        new_image = cv2.bitwise_and(img, img, mask=mask)
+        self.debugImageShow('new-image', new_image)
+
+        (x, y) = np.where(mask == 255)
+        (topx, topy) = (np.min(x), np.min(y))
+        (bottomx, bottomy) = (np.max(x), np.max(y))
+        cropped = gray[topx:bottomx + 1, topy:bottomy + 1]
+        self.debugImageShow('cropped', cropped)
+
+
+if __name__ == "__main__":
+    NumberSearcher = MorphologicalLicenseNumberSearcher(debug=True)
+
+    filename = os.path.join(os.path.dirname(__file__), "objects-sources/img_10.png")
 
     image = cv2.imread(filename)
     NumberSearcher.debugImageShow('car', image)
-    image = imutils.resize(image, width=700, height=700)
+    image = imutils.resize(image, width=600, height=600)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    candidates = NumberSearcher.locateLicensePlateCandidates(gray)
-    NumberSearcher.locateLicensePlate(gray, candidates, True)
+    # candidates = NumberSearcher.locateLicensePlateCandidates(gray)
+    # NumberSearcher.locateLicensePlate(gray, candidates, True)
+
+    CannyNumberSearcher = CannyLicenseNumberSearcher()
+    CannyNumberSearcher.locateLicensePlate(gray, image)
