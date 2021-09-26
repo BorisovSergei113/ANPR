a335ea9b9aca9c84c7d4d58b270ae6e4c2930750
Sergey Borisov <borisovsergei113@mail.ru>
2021-09-26 21:19:56 +0300

    add HaarCascadeLicenseSearcher

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Automatic License/Number Plate Recognition (ANPR) 
 
 [![N|Solid](http://ForTheBadge.com/images/badges/made-with-python.svg)](https://www.python.org/)
+ 
+Предварительно полученные фильтры Хаара - _https://github.com/opencv/opencv/tree/master/data/haarcascades_
+
+Точность определения номера = 82.1%
+
+![alt0](readme-img/1.PNG)
+![alt0](readme-img/2.PNG)
+![alt0](readme-img/3.PNG)
+![alt0](readme-img/4.PNG)
+![alt0](readme-img/5.PNG)
--- main.py
+++ main.py
@@ -1,6 +1,41 @@
-def ANPR():
-    pass
+import os
+import imutils
+import cv2
+
+
+class HaarCascadeRussianPlate:
+    def __init__(self, HaarCascade):
+        self.HaarCascade = HaarCascade
+
+    def debugImageShow(self, title, image, waitKey=True):
+        cv2.imshow(title, image)
+        if waitKey:
+            cv2.waitKey(0)
+
+    def LocateRegionLicensePlate(self, image):
+        carplate = image.copy()
+        # Метод возвращает массив прямоугольников, возможно описывающих номерной знак, и
+        # их нижнюю точку (x, y) ширину и высоту этих прямоугольников
+        # scaleFactor - величина изменяющая размер окна классификатора Хаара
+        # minNeighbors - мин. количество соседних классификаторов для потверждения детектируемого объекта
+        carplateRects = self.HaarCascade.detectMultiScale(carplate, scaleFactor=1.1, minNeighbors=5)
+        for x, y, w, h in carplateRects:
+            cv2.rectangle(carplate, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            self.debugImageShow('plate', carplate)
+            return carplate
+        print("not found")
 
 
 if __name__ == "__main__":
-    ANPR()
+    haarcascade = cv2.CascadeClassifier('./haarcascade_russian_plate_number.xml')
+    HaarCascadeSearcher = HaarCascadeRussianPlate(haarcascade)
+
+    for i in range(1, 29):
+        filename = os.path.join(os.path.dirname(__file__), "objects-sources/img_{}.png".format(i))
+        print("objects-sources/img_{}.png".format(i))
+        image = cv2.imread(filename)
+        image = imutils.resize(image, width=800, height=800)
+
+        # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        HaarCascadeSearcher.LocateRegionLicensePlate(image)
